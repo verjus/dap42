89e430ec9060bdd38c2b6828b6f7bcd64ad2f2d4
Devan Lai <devan.lai@gmail.com>
2018-12-31 12:40:16 -0800

    DAP: pool DAP command request/response buffers

--- src/DAP/app.c
+++ src/DAP/app.c
@@ -26,8 +26,7 @@
 #include "USB/hid.h"
 #include "DAP/app.h"
 
-static uint8_t request_buffers[DAP_PACKET_SIZE][DAP_PACKET_QUEUE_SIZE];
-static uint8_t response_buffers[DAP_PACKET_SIZE][DAP_PACKET_QUEUE_SIZE];
+static uint8_t packet_buffers[DAP_PACKET_SIZE][DAP_PACKET_QUEUE_SIZE];
 
 static uint8_t inbox_tail;
 static uint8_t process_head;
@@ -36,7 +35,7 @@ static uint8_t outbox_head;
 static GenericCallback dfu_request_callback = NULL;
 
 static bool on_receive_report(uint8_t* data, uint16_t len) {
-    memcpy((void*)request_buffers[inbox_tail], (const void*)data, len);
+    memcpy((void*)packet_buffers[inbox_tail], (const void*)data, len);
     inbox_tail = (inbox_tail + 1) % DAP_PACKET_QUEUE_SIZE;
 
     return ((inbox_tail + 1) % DAP_PACKET_QUEUE_SIZE) != outbox_head;
@@ -44,7 +43,7 @@ static bool on_receive_report(uint8_t* data, uint16_t len) {
 
 static void on_send_report(uint8_t* data, uint16_t* len) {
     if (outbox_head != process_head) {
-        memcpy((void*)data, (const void*)response_buffers[outbox_head],
+        memcpy((void*)data, (const void*)packet_buffers[outbox_head],
                DAP_PACKET_SIZE);
         *len = DAP_PACKET_SIZE;
 
@@ -86,15 +85,18 @@ bool DAP_app_update(void) {
     bool active = false;
 
     if (process_head != inbox_tail) {
-        memset(response_buffers[process_head], 0, DAP_PACKET_SIZE);
-        DAP_ProcessCommand(request_buffers[process_head],
-                           response_buffers[process_head]);
+        uint8_t response_buffer[DAP_PACKET_SIZE] = {};
+        DAP_ProcessCommand(packet_buffers[process_head],
+                           response_buffer);
+        // Copy the response back into the slot we used for the request
+        memcpy(packet_buffers[process_head], response_buffer, DAP_PACKET_SIZE);
+        // Mark this slot as pending to send and move onto the next item
         process_head = (process_head + 1) % DAP_PACKET_QUEUE_SIZE;
         active = true;
     }
 
     if (outbox_head != process_head) {
-        if (hid_send_report(response_buffers[outbox_head], DAP_PACKET_SIZE)) {
+        if (hid_send_report(packet_buffers[outbox_head], DAP_PACKET_SIZE)) {
             outbox_head = (outbox_head + 1) % DAP_PACKET_QUEUE_SIZE;
         }
         active = true;
